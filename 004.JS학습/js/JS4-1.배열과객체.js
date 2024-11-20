e81db70e9a04bc5be2f794d785dacbaf93726e22
eddyhwang97 <hwangdw1997@gmail.com>
2024-11-20 20:10:13 +0900

    ,

--- 004.JS학습/js/JS4-1.배열과객체.js
+++ 004.JS학습/js/JS4-1.배열과객체.js
@@ -43,7 +43,82 @@ const arr1 = new Array(
     "리틀 포레스트",
     "정년이",
   ],
-  "김태리"
+  "김태리",
+  function(){
+    // this 는 누구? 호출한 요소 자신
+    // alert("김태리 멋찜!");
+    // 1. 호출한 요소 박스에  김태리 사진을 넣고 
+    this.innerHTML += `
+    <img src="https://img.vogue.co.kr/vogue/2021/02/style_6020aef125358-743x930.jpg" alt="김태리이미지" 
+    style="
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100px;
+    border-radius: 10%;
+    border: 3px solid lightcoral;
+    transition: .4s ease-out;
+    translate : -50% -50%;
+    display: none;
+    pointer-events: none;
+    z-index: 99;
+    "
+    id = "kim";
+
+    >
+    `;
+
+    // 위의 CSS코드 체크!!!
+    // pointer-events: none -> 실제 이벤트는 부모박스인데 
+    // 자식요소인 이미지가 매번 가림으로 인해 이벤트가 
+    // 순간 없어졌다가 다시 걸렸다가함으로 떨림증상발생!!
+    // 이 설정으로 본  이미지는 이벤트를 없애고 아래쪽 요소가
+    // 그대로 이벤트를 발생하게됨! 따라다니는 기능에서
+    // 매우 중요한 설정임!!!!
+
+
+    // 앱솔루트의 부모자격을 this에게 준다.
+    this.style.position = "relative";
+
+    // 위에서 생성된 id="kim" 요소를 변수에 할당한다.
+    const kim = document.querySelector("#kim");
+
+    // 2. 마우시 오버시 나타나고
+    this.onmouseenter = () => {
+      document.querySelector("#kim")
+      .style.display = "block";
+    };
+    // 3. 마우스 아웃시 사라지고
+    this.onmouseleave = () => {
+      document.querySelector("#kim")
+      .style.display = "none";
+    };
+    // 4. 마우스 움직이면 따라다니게해
+    this.onmousemove = (헐) => {
+      // 헐 변수는 이벤트 전달변수임!
+      // 어떤 함수도 전달값이 없는데 변수하나를 쓰면
+      // 곧 그것이 이벤트 전달변수가 됨!
+      // 그요소에서 발생하는 이벤트를 객체로 가지고 있음
+      // 대체해서 evevt라고 직접 전제 이벤트 객체를 쓸 수 있음
+      // 이벤트 객체하위 pageX, pageY는 최상단 , 최왼쪽으로부터 
+      // 마우스 커서의 위치를 x,y축으로 단위없는 px값을 리턴한다.
+      // 이값은 사이트 전체를 이동하는 어떤 요소를 구현할 때 많이 사용한다.
+
+      // console.log("pageX: " , 헐.pageX);
+      // console.log("pageY: " , 헐.pageY);
+
+      // 여기서는 본 박스 안에서만 위치를 알면 되므로 
+      // -> offsetX, offsetY -> 해당 부모박스 요소로부터 위치를 리탄함
+
+      // console.log("offsetX: ", 헐.pageX);
+      // console.log("offsetY: ", 헐.pageY);
+      console.log("offsetX: ", 헐.pageX,"offsetY: ", 헐.pageY);
+
+      // 위치값 반영대상 : 김태리 이미지 -> #kim 
+      kim.style.left = `${헐.offsetX}px`;
+      kim.style.top = `${헐.offsetY}px`;
+    };
+  }
 );
 
 // new키워드로 선언과 할당을 동시에 할 수 있다.
@@ -80,6 +155,16 @@ target[0].innerHTML = `
     대표작: ${arr1[3]}/
     데뷔년도: ${arr1[4][0]}
 `;
+
+
+// 김태리 기능추가
+// 함수호출 : 배열 마지막 번호는 == 배열개수 -1
+target[0].onclick = arr1[arr1.length - 1];
+// click이벤트 강제실행!
+// click()메서드 실행
+target[0].click();
+
+
 // 배열안에 배열이 또있으면 대괄호를 추가하여
 // 해당 순번을 써준다
 
@@ -89,6 +174,20 @@ const arr2 = [
   1919,
   function () {
     alert("대한독립만세");
+    // this는 누구인가? 호출한 요소 자신
+    console.log("this:", this);
+    // 배경넣기
+    this.style.background = `url(https://blog.kakaocdn.net/dn/H4k8p/btqUUqx7TLT/VAMfjsV79wqyKIfOGXn5P0/img.jpg) repeat-x 0/auto 100%`;
+    // 트랜스폼 변경 : 스케일 1.5, 회전 720도
+    this.style.scale = "1.5";
+    this.style.rotate = "720deg";
+    // 트랜지션 : 2초 ease-in-out
+    this.style.transition = `2s ease-in-out`;
+
+    // 5초후에 다시 원래 크기로 돌아가기
+    setInterval(() => {
+      this.style.scale = "1";
+    }, 3000);
   },
 ];
 // 배열끝에 컴마는 원래는 쓰면 에러나지만
@@ -99,11 +198,76 @@ const arr2 = [
 // function() {} 익명함수 -> 이름없고 코드만 저장
 // -> 배열값으로 문자, 숫자, 배열, 객체, 함수 등 사용가능
 
-console.log("arr2는 배열인가?", Array.isArray(arr2)?"응":"아니");
+console.log(
+  "arr2는 배열인가?",
+  Array.isArray(arr2) ? "응" : "아니"
+);
 
 // 출력대상 : target의 두번째
 target[1].innerHTML = `
     ${arr2[0]}은 ${arr2[2]}년에 일제에 항거하여 
-    
-
+    ${arr2[1]}를 들고 일어난 민중봉기를 기념하는 날이다
 `;
+
+// 두번째 출력박스를 클릭하면 배열 함수 호출하기
+target[1].addEventListener(
+  "click",
+  arr2[3]
+);
+// 두번째 박스에 타이틀 출력
+target[1].title =
+  "클릭하시면 만세를 합니다!";
+// 두번째 박스에 손가락 표시
+target[1].style.cursor = "pointer";
+
+// console.log("배열안의 함수 : ",arr2[3]);
+
+// 1.3 배열을 미리 생성하여 각각할당하기 //////
+// 배열변수명 = [] ->배열 리터럴
+// 배열변수명.length = 숫자 -> 숫자만큼 배열이 생성됨
+// 배열변수명.length 는 배열개수를 읽기/쓰기 모두 가능함
+const arr3 =[];
+// const 상수로 리터럴 선언, 할당후
+// 배열값 변경은 자유롭다
+// 그러나 배열형을 변경할 수 없다! 즉, 재할당불가!!
+// 그래서 상수임! (코드보안상, 안전상 이유로 많이 씀)
+
+console.log("arr3 : ",arr3);
+
+// 배열개수 미리 셋팅하기
+arr3.length = 8;  
+// 배열의 개수를 미리 셋팅해도 배열을 더 추가할 수 있음
+// 의미는? 미리 배열개수를 정하고, 이것을 지키려는 의도임
+
+console.log("arr3 : ",arr3);
+
+// 각 배열주소에 값 할당하기
+arr3[0] = "산신령";
+arr3[1] = "할아버지";
+arr3[2] = "구름과자";
+arr3[3] = "잘태우네";
+arr3[4] = "나도한번";
+arr3[5] = "태워볼까";
+arr3[6] = "이거정말";
+arr3[7] = "맛도좋네";
+
+// 배열전체값 추렭하기 : valueOf()
+console.log("arr3배열전체값 : ",arr3.toString());
+console.log("arr3배열전체값 : ",arr3.valueOf().toString());
+// 현재 브라우저는 valueOf()안해도 배열값을 보여준다!
+// toString() 출력은 배열값을 콤마로 연결한 문자열로 변환한다.
+
+// 변수값 사이에 구분자 넣고 문자형으로 배열값 변경하기
+// join(구분자) -> 구분자 넣고 문자값 생성
+console.log("arr3배열 join() : ",arr3.join("🚬💨💨💨"));
+
+// 배열값 맨뒤에 값 추가하기 메서드 : push()
+arr3.push("익명의 나무꾼작사");
+
+// 배열값을 세번째 target박스에 출력하기
+// join()으로 사이에 별표 넣고 출력하기
+target[2].innerHTML = arr3.join("⭐️");
+
+// -> 배열 메서드는 중요하므로 별도로 훈련함!!
+
+
