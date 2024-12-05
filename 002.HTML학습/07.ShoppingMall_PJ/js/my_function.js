91e971b7c1dc04805f945e6aeb150d9387fa29d8
eddyhwang97 <hwangdw1997@gmail.com>
2024-12-05 14:50:17 +0900

    ,

--- 002.HTML학습/07.ShoppingMall_PJ/js/my_function.js
+++ 002.HTML학습/07.ShoppingMall_PJ/js/my_function.js
@@ -1,15 +1,16 @@
 // 나의 함수 객체 ///////
 const myFn = {
-    // 요소 선택함수
-    qs: x => document.querySelector(x),
-    qsEl: (el,x) => el.querySelector(x),
-    qsa: x => document.querySelectorAll(x),
-    qsaEl: (el,e) => el.querySelectorAll(x),
-
-    // 이벤트 셋팅 함수
-    addEvt : (el,evt,fn) => el.addEventListener(evt, fn),
-
-};///// mtFn 객체 /////
+  // 요소 선택함수
+  qs: (x) => document.querySelector(x),
+  qsEl: (el, x) => el.querySelector(x),
+  qsa: (x) =>
+    document.querySelectorAll(x),
+  qsaEl: (el, x) =>
+    el.querySelectorAll(x),
+  // 이벤트 셋팅 함수
+  addEvt: (el, evt, fn) =>
+    el.addEventListener(evt, fn),
+}; ///// mtFn 객체 /////
 
 // import해서 사용할 파일 이므로 객체를 내보내야함!
 export default myFn;
