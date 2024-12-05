3aff30fe6d550f5f74a718bf18ed2a24f3fd680b
eddyhwang97 <hwangdw1997@gmail.com>
2024-12-05 12:57:05 +0900

    ,

--- 002.HTML학습/07.ShoppingMall_PJ/js/main1.js
+++ 002.HTML학습/07.ShoppingMall_PJ/js/main1.js
@@ -12,17 +12,16 @@ import myFn from "./my_function.js";
         (1) 오른쪽 버튼 클릭시 다음 슬라이드가
             나타나도록 슬라이드 박스의 left값을
             -100%로 변경시킨다.
-            -> 슬라이드 이동후!!! 
-            바깥에 나가있는 첫번째 슬라이드
-            li를 잘라서 맨뒤로 보낸다!
-            동시에 left값을 0으로 변경한다!
+            
 
         (2) 왼쪽버튼 클릭시 이전 슬라이드가
             나타나도록 하기위해 우선 맨뒤 li를
             맨앞으로 이동하고 동시에 left값을
             -100%로 변경한다.
-            그 후 left값을 0으로 애니메이션하여
-            슬라이드가 왼쪽에서 들어온다.
+            
+        
+        (3) 이동 중 첫번째 슬라이드는 왼쪽이동버튼 숨기기
+            마지막 슬라이드는 오른쪽 이동버튼 숨기기함
 
         (3) 공통기능: 슬라이드 위치표시 블릿
             - 블릿 대상: .indic li
@@ -31,25 +30,57 @@ import myFn from "./my_function.js";
 
 *****************************************************/
 
-// 전역변수구역 //////////
-
-/* 
-    (참고: JS에서 이름짓는 일반규칙)
-    1. 변수/함수 : 캐믈케이스(첫단어소문자 뒷단어 대문자시작)
-    2. 생성자함수/클래스 : 파스칼케이스(모든첫글자 대문자)
-    3. 상수 : 모든글자 대문자(연결은 언더스코어-스네이크 케이스)
-*/
+// 전역변수 구역 //////////
+// (1) 순번 변수
+let seqNum = 0;
 
 // 초기테스트
 // 오른쪽버튼 클릭시 ul.slide의 css를 변경하여 왼쪽으로 이동
 
-// 이벤트대상: .ab2
-// 변경대상 : .slide
-const slide = myFn.qs('.slide');
+// 1. 대상선정 //////////////////
+// 전체 슬라이드 박스 : .slide-box
+const slideBox = myFn.qs('.slide-box');
+// 전체박스 하위의 요소로 상대적으로 잡아준다
+// 이유는 다른 슬라이드 박스를 카피하여 만든 경우
+// 동일한 기능이 될 수 있게 해준다
+// 이벤트대상: .abtn
+const abtn = myFn.qsaEl(slideBox,'.abtn');
+// 변경대상: .slide
+const slide = myFn.qsEl(slideBox,'.slide');
 
-myFn.qs('.ab2').onclick = () => {
-    // console.log("hi");
-    // 슬라이드 css변경하기
-    slide.style.translate = "-100%";
-    slide.style.transition = ".4s ease-in-out";
+// console.log("대상: ",slideBox,abtn,slide);
+
+// 이벤트 설정하기
+abtn.forEach(el=>{
+    // console.log("요소: ",el)
+    // 각 요소에 이벤트 설정하기
+    // click 이벤트를 addEventListener로 설정
+    myFn.addEvt(el,'click',goSlide);
+});
+function goSlide(){
+    console.log("나함수",this);
+
+    // 2. 오른쪽 버튼 여부 확인
+    let isRight = this.classList.contains('ab2');
+    // classList.contains(클래스명) -> 클래스 있으면 true
+
+    // 3. 분기하여 슬라이드 순번 변수 증감하기
+    // (1) 오른쪽일때 증가
+    if(isRight){
+        seqNum++;
+    }
+    else{
+        seqNum--;
+    }
+};
+
+myFn.qs(".ab2").onclick = () => {
+  // console.log("hi");
+  // 순번1증가
+  seqNum++;
+  // 슬라이드 css변경하기
+  slide.style.translate =
+    seqNum * -100 + "%";
+  slide.style.transition =
+    ".4s ease-in-out";
 };
