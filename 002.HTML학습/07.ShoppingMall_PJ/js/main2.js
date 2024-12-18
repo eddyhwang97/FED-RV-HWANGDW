2626dd6960538a33ef6a6d1d24cc66a3fd082f73
eddyhwang97 <hwangdw1997@gmail.com>
2024-12-18 11:33:11 +0900

    ,

--- 002.HTML학습/07.ShoppingMall_PJ/js/main2.js
+++ 002.HTML학습/07.ShoppingMall_PJ/js/main2.js
@@ -8,18 +8,18 @@
     4. 기능 설계:
 
         (1) 아랫쪽 버튼 클릭시 다음 슬라이드가
-            나타나도록 슬라이드 박스의 top값을
+            나타나도록 슬라이드 박스의 translate y축 방향값을
             -100%로 변경시킨다.
             -> 슬라이드 이동후!!! 
             바깥에 나가있는 첫번째 슬라이드
             li를 잘라서 맨뒤로 보낸다!
-            동시에 top값을 0으로 변경한다!
+            동시에 translate y축 값을 0으로 변경한다!
 
         (2) 윗쪽 버튼 클릭시 이전 슬라이드가
             나타나도록 하기위해 우선 맨뒤 li를
-            맨앞으로 이동하고 동시에 top값을
+            맨앞으로 이동하고 동시에 translate y축 값을
             -100%로 변경한다.
-            그 후 top값을 0으로 애니메이션하여
+            그 후 translate y축 값을 0으로 애니메이션하여
             슬라이드가 왼쪽에서 들어온다.
 
         (3) 공통기능: 슬라이드 위치표시 블릿
@@ -28,3 +28,86 @@
             li에 클래스 "on"주기(나머진 빼기->초기화!)
 
 *****************************************************/
+// 이벤트 대상
+const $btnMove = $(".abtn");
+// 변경대상:  슬라이드박스
+const $slide = $(".slide");
+
+// 광클금지 상태변수
+let stsClick = false;
+
+// 슬라이드 애니시간 상수
+const TIME_ANI = 500;
+
+
+// [2. 이벤트 설정 및 함수구현]
+$btnMove.click(function () {
+    // 광클금지 설정 /////
+    if (stsClick) return; //함수를 나가
+    stsClick = true; //문잠금!
+    setTimeout(() => {
+      stsClick = false; //잠금해제
+    }, TIME_ANI+'ms');
+    ////////////////
+
+  // (1) 방향구분하기 : 아래쪽버튼(.ab2) 이면 true
+  let isbtn = $(this).is(".ab2");
+  console.log("나야나", this, isbtn);
+
+  // (2 방향별 분기하기
+  // (2-1) 윗쪽방향으로 이동하기
+  if (isbtn) {
+    //(2-1-1) 윗쪽 방향으로 이동
+    // 슬라이드의 translate y값을 -100%
+    // 제이쿼리 animate 로 transform은 적용안됨
+    // css로만 적용
+    $slide.css({
+      translate: "0 -100%",
+      transition: TIME_ANI+'ms',
+      // 이동후 잘라내기
+    });
+    setTimeout(() => {
+      $slide
+        // (2-1-2)맨앞것 맨뒤로 이동
+        .append(
+          $slide.find("li").first()
+        )
+        // 이때 translate값 초기화
+        .css({
+          translate: "0 0",
+          transition: "none",
+          // 이동후 잘라내기
+        });
+    }, TIME_ANI+'ms');
+  } ///if ////
+  // (2-2) 윗쪽방향으로 이동하기
+  else {
+    //(2-2-1)맨뒤요소 맨앞으로 이동
+    $slide
+    .prepend($slide.find("li").last())
+    // (이때 translate y 축 값 -100%변경)
+      .css({
+        translate: "0 -100%",
+        transition: "none",
+      })
+      //    (2-2-2) translate y축값을 0으로  위쪽에서 들어오기
+      //    실행구역을 setTimeout으로 분리함
+      setTimeout(() => {
+        $slide.css({
+            translate: "0 0",
+            transition: TIME_ANI+'ms',
+          });
+      }, 0);
+      
+  } ///else ////
+}); ////click
+
+// [3. 처음슬라이드에 고유번호 속성넣기]
+// 제이쿼리 순회 메서드 : each((순번,요소)=>{})
+    $slide.find('li').each((idx,el)=>{
+        console.log('돌아',idx,el)
+        // 각요소에 속성 추가하기
+        // 속성명 : data-seq
+        // 제이쿼리 속성 셋팅 메서드 : attr(속성명,값)
+        $(el).attr('data-seq',idx);
+    }) ///each ////
---
+++ 700.shadowing project/01.DKB_PJ/03.구현소스/js/main.js
@@ -0,0 +1,68 @@
+// 도깨비 PJ 메인페이지 JS - main.js
+
+// 메인배너 슬라이드 기능함수 불러오기
+import slideFn from "./slide_fn.js";
+
+import {previewData} from "../data/dkb_data.js"
+console.log(previewData)
+// console.log(slideFn);
+// 슬라이드함수 호출하여 실행하기
+slideFn();
+
+// 2. 데이터 셋팅하기
+// (1) 미리보기 데이터 셋팅하기
+//  대상: .previewBox ul
+// 데이터 : previewdata
+// map 메서드 사용하기
+// 배열값 변령출력은? 맵죠잉!
+// 원래는 일반 js에서 html 넣을때 
+// 배열값 직접 넣으면 사이에 콤마가 출력됨
+// 그래서 join('')으로 없애고 출력함!
+// ->제이쿼리 html() 메서드에서는 join()없이
+// 콤마없애고 출력해줌!!
+
+$('.preview-box ul').html(
+    previewData.map(v=>`
+        <li>
+            <h3>${v.title}</h3>
+            <p>${v.story}</p>
+        </li>      
+        `).join('')
+)
+
+//스와이퍼 인스턴스 생성하기
+const swiper = new Swiper(".clip-box", {
+    // 한화면에 볼 슬라이드수
+    slidesPerView: 4,
+    // 슬라이드 사이 간격(숫자는 px단위임)
+    spaceBetween: 0,
+    
+  }); /// swiper /////
+
+  // 스와이퍼 API를 이용한 개별 코딩
+        // 내가 만든 버튼에 이동 기능 부여하기!
+        const myBtns = document.querySelectorAll(".btn-box button");
+        console.log("대상:", myBtns);
+
+        // 클릭이벤트 설정하기
+        myBtns.forEach((el) => {
+          el.addEventListener("click", controlSwp);
+        });
+
+        // 기능구현함수
+        function controlSwp() {
+          // 버튼구분하기
+          let isBtn = this.classList.contains("btn-next");
+          console.log("나야나!", isBtn);
+
+          // 분기하기
+          // 다음버튼
+          if (isBtn) {
+            swiper.slideNext();
+          } // if //
+          // 이전버튼
+          else {
+            swiper.slidePrev();
+          } // else //
+        } ////////// controlSwp함수 /////////
+
