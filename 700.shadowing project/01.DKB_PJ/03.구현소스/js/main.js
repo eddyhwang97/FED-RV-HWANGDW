7a763b03f0f02b4c472b922046c392df79b52ead
eddyhwang97 <hwangdw1997@gmail.com>
2024-12-31 01:55:22 +0900

    ,

--- 700.shadowing project/01.DKB_PJ/03.구현소스/js/main.js
+++ 700.shadowing project/01.DKB_PJ/03.구현소스/js/main.js
@@ -5,11 +5,54 @@ import slideFn from "./slide_fn.js";
 
 // 도깨비 PJ 데이터 불러오기
 // import { previewData } from "../data/dkb_data.js";
-import * as dkbData from "../data/dkb_data.js";
+// import * as dkbData from "../data/dkb_data.js";
 // 넘겨준 것을 모두 받는 방법은 별(*)로 받고
 // as로 별칭을 지어주면 객체화되어 담겨진다.
 
-console.log(dkbData);
+// console.log(dkbData);
+
+// 도깨비 GNB데이터 불러오기
+import gnbData from "../data/gnb_data.js";
+console.log(gnbData, Object.keys(gnbData), gnbData["About tvN"]);
+
+// 0. GNB데이터 바인딩하기
+$(".gnb").html(`
+  <ul class="fx-box">
+  ${Object.keys(gnbData)
+    .map(
+      (v) => `
+    <li>
+      <a href="#">
+        ${v + (gnbData[v] == "없음" ? "" : '<i class="fa-solid fa-chevron-down"></i>')}
+        </a>
+        ${
+          gnbData[v] == "없음"
+            ? ""
+            : `
+        <!-- 서브메뉴 -->
+      <aside class="smenu">
+        <div class="inbox">
+          <h2>${v}</h2>
+          <ol>
+          ${gnbData[v].map(
+            (v2) => `
+          <li>
+            <a href="#">${v2}</a>
+          </li>
+          `
+          ).join('')}
+          </ol>
+        </div>
+      </aside>
+        `
+        }
+    </li>                                       
+    `
+    )
+    .join("")}
+  </ul>
+  `);
+
 // console.log(slideFn);
 // 슬라이드함수 호출하여 실행하기
 slideFn();
@@ -100,8 +143,9 @@ $(".clip-box").html(
       class="slide swiper-wrapper"
       data-db="clipData"
     >
-    ${dkbData.clipData.map(
-      (v) => `
+    ${dkbData.clipData
+      .map(
+        (v) => `
       <li
         class="swiper-slide"
         data-idx="${v.idx}}"
@@ -121,7 +165,8 @@ $(".clip-box").html(
         <h3>${v.title}}</h3>
       </li>
       `
-    ).join('')}
+      )
+      .join("")}
   </ul>
                 `
 );
@@ -139,25 +184,18 @@ const swiper = new Swiper(".clip-box", {
 
 // 스와이퍼 API를 이용한 개별 코딩
 // 내가 만든 버튼에 이동 기능 부여하기!
-const myBtns =
-  document.querySelectorAll(
-    ".btn-box button"
-  );
+const myBtns = document.querySelectorAll(".btn-box button");
 console.log("대상:", myBtns);
 
 // 클릭이벤트 설정하기
 myBtns.forEach((el) => {
-  el.addEventListener(
-    "click",
-    controlSwp
-  );
+  el.addEventListener("click", controlSwp);
 });
 
 // 기능구현함수
 function controlSwp() {
   // 버튼구분하기
-  let isBtn =
-    this.classList.contains("btn-next");
+  let isBtn = this.classList.contains("btn-next");
   console.log("나야나!", isBtn);
 
   // 분기하기
@@ -179,9 +217,7 @@ function controlSwp() {
 const $subCont = $(".sub-cont");
 
 // 닫기버튼 셋팅
-$subCont
-  .find(".cbtn")
-  .click(() => $subCont.fadeOut());
+$subCont.find(".cbtn").click(() => $subCont.fadeOut());
 
 // 1. 미리보기영역 클릭시 세부내용 보기
 //  이벤트 대상 : .preview-box li
@@ -194,9 +230,7 @@ $(".preview-box li").click(function () {
 
   // 2. 읽어온 내용을 서브컨텐츠 박스에 넣기
   $subCont.find("h1").text(currTit);
-  $subCont
-    .find("내용박스")
-    .text(currCont);
+  $subCont.find("내용박스").text(currCont);
   // 3. 서브컨텐츠 박스 보이기
   $subCont.fadeIn();
   // fadeIn(시간) 시간 안쓰면 400
