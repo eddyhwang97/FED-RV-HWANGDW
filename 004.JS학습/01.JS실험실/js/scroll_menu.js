d0a761d16c9ccc77053fa2281eabf1272e2b7ab0
eddyhwang97 <hwangdw1997@gmail.com>
2025-01-09 15:43:48 +0900

    ,

--- 004.JS학습/01.JS실험실/js/scroll_menu.js
+++ 004.JS학습/01.JS실험실/js/scroll_menu.js
@@ -1,2 +1,16 @@
 // 스크롤 방향에 따른 메뉴 보이기/숨기기 JS
 // - scroll_menu.js
+
+import myFn from "./my_function.js";
+
+const headerMenu = myFn.qs('#header')
+
+myFn.addEvt(window, "wheel", (e) => {
+    let delta = e.wheelDelta
+  console.log("휠", delta);
+
+//   wheeldelta 값이 마이너스는 아랫방향
+// ->#header에 .on을 줘서 숨기기
+if(delta < 0) headerMenu.classList.add('on');
+else headerMenu.classList.remove('on');
+});
