b672315949d5cf0bccf4f32aa62ea68c69f736b9
eddyhwang97 <hwangdw1997@gmail.com>
2025-04-15 13:06:37 +0900

    ㅇ

---
+++ 9994.typescript/dist/bundle.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+
+function greet(name) {
+    return `안녕, ${name}~!`;
+}
+console.log(greet("찐친 개발자"));
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
