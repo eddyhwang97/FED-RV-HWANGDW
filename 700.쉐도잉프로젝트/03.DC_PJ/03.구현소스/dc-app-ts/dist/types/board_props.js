b87e5186c6c02e0200f4e8fbbfbb5bc757442a3e
eddyhwang97 <hwangdw1997@gmail.com>
2025-04-22 13:25:55 +0900

    ㅎ

--- 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app-ts/dist/types/board_props.js
+++
@@ -1,2 +0,0 @@
-// 게시판 관련 타입정의 파일 - borad_props.ts
-export {};
--- 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app-ts/src/js/data/vid_swipe.js
+++
@@ -1,5 +0,0 @@
-// 비디오스와이프 카테고리별 타이틀
-export const catTit = {
-    main:"LATEST TRAILERS, CLIPS & MORE",
-    MOVIES:"TRAILERS, CLIPS AND MORE",
-};
